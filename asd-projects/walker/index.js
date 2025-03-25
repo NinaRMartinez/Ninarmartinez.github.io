d0ca4e3bf63b99209393e4a8c81a6a2943239a9d
NinaRMartinez <martin114@wearescpps.org>
2025-03-25 16:21:28 +0000

    done

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -1,5 +1,3 @@
-/* global $, sessionStorage */
-
 $(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
   
 function runProgram(){
@@ -10,14 +8,26 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+  var KEY = {
+    LEFT: 37,
+    RIGHT: 39,
+    DOWN: 40,
+    UP: 38
+  }
   
   // Game Item Objects
-
+  var walker = {
+    walkerX: 0,
+    walkerY: 0,
+    speedX: 0,
+    speedY: 0
+  }
 
   // one-time setup
-  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
-
+  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame at 60fps
+  $(document).on('keydown', handleKeyDown);                           // handling key down events
+  $(document).on('keyup', handleKeyUp);                               
+  $("#walker").on('click', changeColor);                              
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -27,17 +37,52 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
+    repositionGameItem(walker);
+    wallCollision(walker);
+    redrawGameItem(walker);
     
-
+   
   }
   
-  /* 
-  Called in response to events.
-  */
-  function handleEvent(event) {
-
+  
+  function handleKeyDown(event) {
+    if (event.which === KEY.LEFT) {
+      walker.speedX = -5;
+      console.log("LEFT pressed");
+    }
+    if (event.which === KEY.UP) {
+      walker.speedY = -5;
+      console.log("UP pressed");
+    }
+    if (event.which === KEY.RIGHT) {
+      walker.speedX = 5;
+      console.log("RIGHT pressed");
+    }
+    if (event.which === KEY.DOWN) {
+      walker.speedY = 5;
+      console.log("DOWN pressed");
+    }
+    
   }
-
+  
+  function handleKeyUp(event){
+    if (event.which === KEY.LEFT) {
+      walker.speedX = 0;
+      console.log("LEFT released");
+    }
+    if (event.which === KEY.UP) {
+      walker.speedY = 0;
+      console.log("UP released");
+    }
+    if (event.which === KEY.RIGHT) {
+      walker.speedX = 0;
+      console.log("RIGHT released");
+    }
+    if (event.which === KEY.DOWN) {
+      walker.speedY = 0;
+      console.log("DOWN released");
+    }
+  }
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -50,5 +95,51 @@ function runProgram(){
     // turn off event handlers
     $(document).off();
   }
+ 
+  function repositionGameItem(item) {
+    //pulling all relevant data
+    let speedX = item.speedX;
+    let speedY = item.speedY;
+
+    item.walkerX += speedX;
+    item.walkerY += speedY;
+  }
+ 
+  function redrawGameItem(item) {
+    //utilizes relavent data, except speed
+    let walkerX = item.walkerX;
+    let walkerY = item.walkerY;
+    
+    $("#walker").css("left", walkerX);
+    $("#walker").css("top", walkerY);
+  }
   
+  function wallCollision(item) {
+    //pulls relevant data and makes containers.
+    let walkerX = item.walkerX;
+    let walkerY = item.walkerY;
+    let speedX = item.speedX;
+    let speedY = item.speedY;
+    let width = $("#board").width() - 45;
+    let height = $("#board").height() - 45;
+
+    if (walkerX < 0) {
+      //collide with left wall
+      item.walkerX -= speedX;
+    }
+    if (walkerY < 0) {
+      //collide with top wall
+      item.walkerY -= speedY;
+    }
+    if (walkerX > width) {
+      //collide with right wall
+      item.walkerX -= speedX;
+    }
+    if (walkerY > height) {
+      //collide with bottom wall
+      item.walkerY -= speedY;
+    }
+  }
 }
+
+  
