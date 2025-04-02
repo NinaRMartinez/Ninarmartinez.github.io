6d89f70169c3602d2b4780962c452666521fd685
NinaRMartinez <martin114@wearescpps.org>
2025-04-02 16:36:30 +0000

    problem child

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,8 +20,9 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-applyFilter(reddify)
-  
+  //applyFilter(reddify);
+  applyFilterNoBackground(decreaseBlue);
+  // applyFilterNoBackground(increaseGreenByBlue);
 
   // do not change the below line of code
   render($("#display"), image);
@@ -32,40 +33,51 @@ applyFilter(reddify)
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
-function applyFilter(filterFunction){
-  for(var i  = 0; i < image.length; i++){
-    for(var j = 0; j < image[i].length; i++){
-      var rgbString = image[i][j]
-      if(background !== rgbString){
-      var rgbNumbers = rgbStringToArray(rgbString)
+function applyFilter(filterFunction) {
+  for (var i = 0; i < image.length; i++) {
+    var currentString = image[i];
+    for (var j = 0; j < currentString.length; i++) {
+      var rgbString = currentString[j];
+      var rgbNumbers = rgbStringToArray(rgbString);
       filterFunction(rgbNumbers);
-    rgbString = rgbArrayToString(rgbNumbers)
+      rgbString = rgbArrayToString(rgbNumbers);
+      image[i][j] = rgbString;
     }
-    image[i][j] = rgbString;
   }
 }
-}
 // TODO 7: Create the applyFilterNoBackground function
-
+function applyFilterNoBackground(filterFunction) {
+  var NoFilterBackground = image[0][0];
+  for (var j = 0; j < image.length; j++) {
+    for (var k = 0; k < image[j].length; k++) {
+      var rgbString = image[j][k];
+      if (NoFilterBackground !== rgbString) {
+        var rgbNumbers = rgbStringToArray(rgbString);
+        filterFunction(rgbNumbers);
+        rgbString = rgbArrayToString(rgbNumbers); 
+        image[j][k] = rgbString;
+      }
+     
+    }
+  }
+}
 
 // TODO 5: Create the keepInBounds function
-function keepInBounds(number){
-  return (number < 0 ? 0 : number > 255 ? 255 : number);
-  
-
+function keepInBounds(number) {
+  return number < 0 ? 0 : number > 255 ? 255 : number;
 }
 
 // TODO 3: Create reddify function
-function reddify(reddifyArray){
-  reddfyArray[RED] = 200
+function reddify(reddifyArray) {
+  reddifyArray[RED] = 200;
 }
 
 // TODO 6: Create more filter functions
-function decreaseBlue(blueArray){
-var newBlue = blueArray[BLUE] - 50;
-keepInBounds(newBlue);
-
-
+function decreaseBlue(blueArray) {
+  blueArray[BLUE] = keepInBounds(blueArray[BLUE]);
+}
+function increaseGreenByBlue(GBBArray) {
+  GBBArray[GREEN] = keepInBounds(GBBArray[GREEN]);
 }
 
 // CHALLENGE code goes below here
