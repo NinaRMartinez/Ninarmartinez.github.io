79936bcb581ccaa7a9cf4a389533f2e912e91365
NinaRMartinez <martin114@wearescpps.org>
2025-04-30 16:57:16 +0000

    todo 4 pt2

--- asd-projects/sorting/index.js
+++ asd-projects/sorting/index.js
@@ -14,16 +14,79 @@ The CSS ids you will work with are:
 ///////////////////////////////////////////////////////////////////////
 
 // TODO 2: Implement bubbleSort
+async function bubbleSort(array){
+//ITERATE over the array from i = 0 to i = length - 1
+for(var i = 0; i <= array.length - 1; i++){
+    // ITERATE over the array from j = length - 1 to j = i + 1
+    for(var j = array.length - 1; j >= i + 1; j--){
+        // IF array[j]'s value < array[j - 1]'s value
+        if(array[j].value < array[j - 1].value){
+// swap array[j] and array[j - 1]
+swap(array,j,(j-1));
+updateCounter(bubbleCounter);
+await sleep();
+        }
+    }
+}
 
+
+  
+}
 
 // TODO 3: Implement quickSort
-
+async function quickSort(left, right, array){
+    if((right - left) > 0){
+        var i = await partition(array,left,right);
+        if(left < (i - 1)){
+            await quickSort(array,left,i - 1);
+        }
+        if(i < right){
+            await quickSort(array,i,right);
+        }
+    }
+    return;
+}
 
 // TODOs 4 & 5: Implement partition
+async function partition(left, right, array){
+    // FUNCTION partition (array, left, right):
 
+  //pivot = select a pivot
+  pivot = array[Math.floor((right + left) / 2)].value;
+
+  //WHILE left < right:
+  while(left < right){
+
+//WHILE array[left] < pivot { left++ }
+while(array[left] < pivot) {left++}
+
+    //WHILE array[right] > pivot { right-- }
+    while(array[left] > pivot){right --}
+
+         //IF left < right:
+        if(left < right){
+
+            //swap array[left] and array[right]
+            swap(array,left, right);
+            updateCounter(quickCounter); //update the quicksort move counter
+            await sleep();
+    }
+   
+
+    
+  }
+    
+
+  //RETURN left + 1
+}
 
 // TODO 1: Implement swap
-
+function swap(i, j, array) {
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+    drawSwap(i, j, array);
+}
 
 ///////////////////////////////////////////////////////////////////////
 /////////////////////// YOUR WORK GOES ABOVE HERE /////////////////////
