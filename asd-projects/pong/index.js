1f277cc1666c63313dc45918577bb9c8ded19519
NinaRMartinez <martin114@wearescpps.org>
2025-05-08 16:54:47 +0000

    week 2 move ball partialy done

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -52,3 +52,64 @@ function runProgram(){
   }
   
 }
+function makeGameItem(id){
+  var gameItem = {}
+  gameItem.x = parseFloat($(id).css("left"));
+  gameItem.y = parseFloat($(id).css("top"));
+  gameItem.id = parseFloat($(id).css("id"));
+  gameItem.width = parseFloat($(id).css("width"));
+  gameItem.height = parseFloat($(id).css("height"));
+  gameItem.speedX = 1;
+  gameItem.speedY = 1;
+}
+var KEY = { // magic numbers
+  LEFT: 37,
+  RIGHT: 39,
+  DOWN: 40,
+  UP: 38
+}
+function handleKeyDown(event) { // handles all keydown events for arrows
+  if (event.which === KEY.LEFT) {
+    ball.speedX = -5;
+    console.log("LEFT pressed");
+  }
+  if (event.which === KEY.UP) {
+    ball.speedY = -5;
+    console.log("UP pressed");
+  }
+  if (event.which === KEY.RIGHT) {
+    ball.speedX = 5;
+    console.log("RIGHT pressed");
+  }
+  if (event.which === KEY.DOWN) {
+    ball.speedY = 5;
+    console.log("DOWN pressed");
+  }
+  
+}
+//handles movement on keyup
+function handleKeyUp(event){ // handles all keyup events for arrows
+  if (event.which === KEY.LEFT) {
+    ball.speedX = 0;
+    console.log("LEFT released");
+  }
+  if (event.which === KEY.UP) {
+    ball.speedY = 0;
+    console.log("UP released");
+  }
+  if (event.which === KEY.RIGHT) {
+    ball.speedX = 0;
+    console.log("RIGHT released");
+  }
+  if (event.which === KEY.DOWN) {
+    ball.speedY = 0;
+    console.log("DOWN released");
+  }
+}
+function startBall(){
+  var ball = {}
+  ball.startingPositionX = 2;
+  ball.startingPositionY = 2;
+  ball.speed = 1
+}
+//THIS WHERE U STOPPED MAY8
